0ef1d07d36f26162dfbc73c7af6b3724c2d13c10
jdryden <dryden.jm@gmail.com>
2014-02-01 19:06:51 -0800

    parse error

--- ASTAR-ToDistribute1/AStarExp2.java
+++ ASTAR-ToDistribute1/AStarExp2.java
@@ -4,10 +4,11 @@ import java.util.List;
 import java.util.Iterator;
 
 // A-Star implementation
-// @authors: Abilio Oliveira and James Ryden
+// @authors: Abilio Oliveira and James Dryden
 public class AStarExp2 implements AIModule
 {
-    public class Node{
+    public class Node
+    {
 	public Point point;
 	public Node parent;
 	public double g;
@@ -18,7 +19,7 @@ public class AStarExp2 implements AIModule
 	}
 
 	public Node(final TerrainMap map, Point point, AStarExp2.Node parent, Double g){
-	    this.point = new Point(point);`
+	    this.point = new Point(point);
 	    this.parent = parent;
 	    this.g = g;
 	    this.f = getHeuristics(map, this.point, map.getEndPoint());
@@ -31,41 +32,45 @@ public class AStarExp2 implements AIModule
 	    this.f = this.g + getHeuristics(map, this.point, map.getEndPoint());
 
 	    return;
-	}
     }
 
     /// AStar algorithm
 
-    public List<Point> createPath(final TerrainMap map) {
-   	AStarExp2.Node node = new AStarExp2.Node(map, map.getStartPoint(), null, 0.0);
-	ArrayList<AStarExp2.Node> frontier = new ArrayList<AStarExp2.Node>();
-	final ArrayList<AStarExp2.Node> explored = new ArrayList<AStarExp2.Node>();
-	Point neighbors[] = new Point[8];
+    public List<Point> createPath(final TerrainMap map)
+    {
+   		AStarExp2.Node node = new AStarExp2.Node(map, map.getStartPoint(), null, 0.0);
+		ArrayList<AStarExp2.Node> frontier = new ArrayList<AStarExp2.Node>();
+		final ArrayList<AStarExp2.Node> explored = new ArrayList<AStarExp2.Node>();
+		Point neighbors[] = new Point[8];
 
-	frontier.add(node);//add map parameter
+		frontier.add(node);//add map parameter
+		AStarExp2.Node probe = null;
+		while(!frontier.isEmpty())
+		{
+			probe = null;
+			probe = frontier.remove(0);
+		    explored.add(0, (probe));
+		    if (probe.point.equals(map.getEndPoint()))
+		    {
+				return this.reconstructPath(explored, probe);
+	    	}
+	    	neighbors = map.getNeighbors(probe.point);
 
-	while(!frontier.isEmpty()){
-		AStarExp2.Node probe = frontier.remove(0);
-	    explored.add(0, new AStarExp2.Nde(probe);
-	    if (probe.point.equals(map.getEndPoint()))
-	    {
-			return this.reconstructPath(explored, probe);
-	    }
-	    neighbors = map.getNeighbors(probe.point);	    
-	    for(int i=0; i<8; i++)
-	    {
-	    	AStarExp2.Node child = new AStarExp2.Node(map, neighbors[i], explored.get(0), map.getCost(probe.point, child.point));
-			if (!frontier.contains(child) && !explored.contains(child))
-			{
-		    	frontier = this.insertionSort(frontier, child);
-			}
-	    }
-	}
+	    	for(int i=0; i<8; i++)
+	    	{
+	    		probe = new AStarExp2.Node(map, neighbors[i], explored.get(0), map.getCost(explored.get(0).point, neighbors[i]));
+				if (!frontier.contains(probe) && !explored.contains(probe))
+				{
+			    	frontier = this.insertionSort(frontier, probe);
+				}
+	    	}
+		}
 
 	return null;// failure
     }
 
-    private double getHeuristics(final TerrainMap map, final Point pt1, final Point pt2) {
+    private double getHeuristics(final TerrainMap map, final Point pt1, final Point pt2)
+    {
 		double hight_difference = map.getTile(pt2) - map.getTile(pt1);
 		int y_difference = pt2.y - pt1.y;
 		int x_difference = pt2.x - pt1.x;
@@ -78,24 +83,29 @@ public class AStarExp2 implements AIModule
     }
 
 
-	public ArrayList<AStarExp2.Node> insertionSort(ArrayList<AStarExp2.Node> frontier, AStarExp2.Node child){
+	public ArrayList<AStarExp2.Node> insertionSort(ArrayList<AStarExp2.Node> frontier, AStarExp2.Node child)
+	{
 		Iterator<AStarExp2.Node> itr = frontier.iterator();
 		int i=0;
-		while(itr.hasNext()) {
+		while(itr.hasNext())
+		{
 		    AStarExp2.Node object = itr.next();
-		    if (child.f < object.f){
+		    if (child.f < object.f)
+		    {
 				break;
+			}
 	    }
 	    i++;
-	}
 	    frontier.add(i, child);
 		return frontier;
     }
 
-    public List<Point> reconstructPath(ArrayList<AStarExp2.Node> explored, AStarExp2.Node n) {
+    public List<Point> reconstructPath(ArrayList<AStarExp2.Node> explored, AStarExp2.Node n)
+    {
 		List<Point> path = new ArrayList<Point>();
 
-		while(!(n.parent == null)) {
+		while(!(n.parent == null))
+		{
 			path.add(0, n.point);
 			n = search_explored(explored, n);
 		}
@@ -103,9 +113,13 @@ public class AStarExp2 implements AIModule
 	}
 
 
-	public Node search_explored(ArrayList<AStarExp2.Node> explored, AStarExp2.Node n) {
+	public Node search_explored(ArrayList<AStarExp2.Node> explored, AStarExp2.Node n)
+	{
 		for(int i = 0; i < explored.size(); i++)
+		{
 			if(explored.get(i).point == n.point)
 				return explored.get(i).parent;
+			else return null;
+		}
 	}
 }
