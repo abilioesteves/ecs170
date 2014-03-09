c713ea71ae241924d1fcc0be723c0dda32b3efd0
Abilio Esteves Calegario de Oliveira <eco.abilio@gmail.com>
2014-03-09 12:30:44 -0700

    3rd assignment repository configuration

---
+++ 3rdAssignment/GenderClassifier/AbilioOliveira_JamesDryden.java
@@ -0,0 +1,19 @@
+import javax.swing.*
+import java.util.*
+
+
+/// General Class.
+/**
+* Due to assignment constraints, all classes will be nested to this class.
+*
+* @author Abilio Oliveira and James Dryden
+*/
+
+public cass AbilioOliveira_JamesDryden {
+	public class Sigmoid{
+
+	}
+	public class Main{
+
+	}
+}
