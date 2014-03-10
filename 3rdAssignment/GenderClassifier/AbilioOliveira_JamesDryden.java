5b8e63651505edfc488865f1b898624108e08aad
Abilio Esteves Calegario de Oliveira <eco.abilio@gmail.com>
2014-03-10 09:29:32 -0700

    Basic project structure. It's compiling.

--- 3rdAssignment/GenderClassifier/AbilioOliveira_JamesDryden.java
+++ 3rdAssignment/GenderClassifier/AbilioOliveira_JamesDryden.java
@@ -1,6 +1,5 @@
-import javax.swing.*
-import java.util.*
-
+import javax.swing.*;
+import java.util.*;
 
 /// General Class.
 /**
@@ -10,42 +9,69 @@ import java.util.*
 */
 
 public class AbilioOliveira_JamesDryden {
-	final int MALE = 1;
-	final int FEMALE = -1;
-	/// Sigmoid unit class
+	final int MALE = 1; // 100% confidence that a test is MALE
+	final int FEMALE = -1; // 100% confidence that a test is FEMALE
+
+	/// Program startup method
 	/**
-	* Holds the structure and functions related to the sigmoid units
+	* Parses command line arguments and set up our Classifier
 	*/
-	public class Sigmoid{
-		public ArrayList<int> weights = new ArrayList<int>();
+	public static void main(String[] args){
+		ANN ann;
+		int result;
+		int i = 0;
+		while (i < args.length){
+			if (args[i].equalsIgnoreCase("-train")) {
+				// perform training
+				ann = new ANN();
+				result = Classifier.train(ann);
+
+			} else if (args[i].equalsIgnoreCase("-test")){
+				// perfrom testing
+				ann = new ANN(args[i+1]); // initialize from file
+				result = Classifier.test(ann);
+			}
+		}
 	}
 
 	/// Classifier class
 	/**
-	* Encapsulates the network structure and deals with its behavior
+	* Computes over the ANN; or computes THE ANN
 	*/
-	public class Classifier{
+	public static class Classifier{
+
+		public static int train(ANN ann){
+			return 0;
+		}
+
+		public static int test(ANN ann){
+			return 0;
+		}
 
 	}
 
-	/// Entry Point class
 	/**
-	* Parses command line arguments and set up our Classifier
+	* Holds the Artificial Neural Network (ANN) structure and its methods
 	*/
-	public class Main{
-		
-		/// Program startup method
-		public static void main(String[] args){
-			try{
-				int i = 0;
-				while (i < args.length){
-					if (args[i].equalsIgnoreCase("-train")) {
-						// perform training
-					} else if (args[i].equalsIgnoreCase("-test")){
-						// perfrom testing
-					}
-				}
-			}
+	public static class ANN{
+		public ArrayList<SigmoidUnit> sigmoids = new ArrayList<SigmoidUnit>(); // maybe a graph instead of an ArrayList? This needs some discussion.
+
+		public ANN (){
+
 		}
+
+		public ANN (String file){
+
+		}
+	}
+
+	/// Sigmoid unit class
+	/**
+	* Holds the structure and methods related to the sigmoid units
+	*/
+	public class SigmoidUnit{
+		public ArrayList<Integer> weights = new ArrayList<Integer>();
+		public double output = 0.0;
+
 	}
 }
